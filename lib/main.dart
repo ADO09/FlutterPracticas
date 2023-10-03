986c22d4829c8a593cff751b988814747bcfd0d4
ADO09 <jai_car_or11@hotmail.com>
2023-10-02 23:07:51 -0600

    asdsad

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:practicas_flutter/src/shared_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp( MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return  MaterialApp(
+ 
+      title: 'Practica 11',
+       home: SharedPage(),
     );
   }
 }
