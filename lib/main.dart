edc610f990742fba43b4a9798b2361e811e23ce9
ADO09 <jai_car_or11@hotmail.com>
2023-10-15 20:34:37 -0600

    asdasd

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:practicas_flutter/src/crud_page.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+
+void main() =>  runApp( MainApp());
+
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Material App",
+     home: CrudPage(),
     );
   }
 }
