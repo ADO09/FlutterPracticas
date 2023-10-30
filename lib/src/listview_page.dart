a4789569bcb0605cd6b72f332bfac46bf370aca7
ADO09 <jai_car_or11@hotmail.com>
2023-10-30 10:13:25 -0600

    asiodjas

---
+++ lib/src/listview_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:practicas_flutter/src/animated_page.dart';
+import 'package:practicas_flutter/src/tween_page.dart';
+
+class ListViewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practica 16 - Animaciones'),
+        centerTitle: true,
+      ),
+      body: ListView(children: [
+        ListTile(
+            title: Text('Animated'),
+            trailing: Icon(Icons.arrow_right),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AnimatedPage()));
+            }),
+        ListTile(
+          title: Text('Tween'),
+          trailing: Icon(Icons.arrow_right),
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => TweenPage()));
+          },
+        ),
+      ]),
+    );
+  }
+}
