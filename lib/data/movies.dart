b03c29c0b789ca468e1d5adb6ce921209a6c92f2
ADO09 <jai_car_or11@hotmail.com>
2023-11-05 17:39:24 -0600

    akosd

---
+++ lib/data/movies.dart
@@ -0,0 +1,54 @@
+final List<Map <String, dynamic>> movies =[
+
+{
+  "id": 1,
+  "title_name": "Pressed",
+  "genres": "Action|Crime|Drama|Thriller",
+  "image": "http://dummyimage.com/184x221.png/cc0000/ffffff"
+}, {
+  "id": 2,
+  "title_name": "Only the Young",
+  "genres": "Documentary",
+  "image": "http://dummyimage.com/244x179.png/dddddd/000000"
+}, {
+  "id": 3,
+  "title_name": "India Song",
+  "genres": "Drama|Fantasy|Romance",
+  "image": "http://dummyimage.com/119x100.png/ff4444/ffffff"
+}, {
+  "id": 4,
+  "title_name": "Sign of the Cross, The",
+  "genres": "Drama",
+  "image": "http://dummyimage.com/222x230.png/dddddd/000000"
+}, {
+  "id": 5,
+  "title_name": "Van Diemen's Land",
+  "genres": "Drama",
+  "image": "http://dummyimage.com/109x147.png/ff4444/ffffff"
+}, {
+  "id": 6,
+  "title_name": "Cheerful Weather for the Wedding",
+  "genres": "Comedy|Drama",
+  "image": "http://dummyimage.com/213x219.png/5fa2dd/ffffff"
+}, {
+  "id": 7,
+  "title_name": "Straw Dogs",
+  "genres": "Drama|Thriller",
+  "image": "http://dummyimage.com/102x170.png/ff4444/ffffff"
+}, {
+  "id": 8,
+  "title_name": "Confucian Confusion, A (Du li shi dai)",
+  "genres": "Comedy",
+  "image": "http://dummyimage.com/152x158.png/ff4444/ffffff"
+}, {
+  "id": 9,
+  "title_name": "High Fidelity",
+  "genres": "Comedy|Drama|Romance",
+  "image": "http://dummyimage.com/201x249.png/5fa2dd/ffffff"
+}, {
+  "id": 10,
+  "title_name": "Lone Wolf and Cub: Baby Cart at the River Styx (Kozure Ôkami: Sanzu no kawa no ubaguruma)",
+  "genres": "Action|Adventure",
+  "image": "http://dummyimage.com/125x123.png/ff4444/ffffff"
+}
+];
